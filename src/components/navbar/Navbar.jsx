77f48a38d72b86cd4e08fa9b4163035dd86078c2
rehmangour04 <rehmangour04@gmail.com>
2022-08-10 22:47:19 +0530

    after change

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">lamabooking</span>
+        <span className="logo">RMnBooking</span>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
